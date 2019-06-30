56192fdfccc8e4e3bef9d8b378dc2dd3ef51a369
aceprokute1 <aceprokute1@gmail.com>
2019-06-30 15:52:07 +0700

    Updates

---
+++ precache-manifest.7167d69f4597ba34dd303a15b86cc244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb1fb22f6745d772c215154e81691c1",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "79e49bfa399c43000fd0",
+    "url": "/birthday/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "02bc33867b4cb3bdd99e",
+    "url": "/birthday/static/css/main.ae61d2da.chunk.css"
+  },
+  {
+    "revision": "79e49bfa399c43000fd0",
+    "url": "/birthday/static/js/2.9c9fb170.chunk.js"
+  },
+  {
+    "revision": "02bc33867b4cb3bdd99e",
+    "url": "/birthday/static/js/main.9eff17af.chunk.js"
+  },
+  {
+    "revision": "fb2cde24ba4de20a670e",
+    "url": "/birthday/static/js/runtime~main.54c049e0.js"
+  }
+]);
